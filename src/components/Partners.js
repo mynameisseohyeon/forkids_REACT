da00a5281f9c35a059d994ed8739beff29510caf
seohyeon <uimi3606@gmail.com>
2023-07-12 22:34:55 +0900

    DESIGN partner 구성

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -4,7 +4,17 @@ function Partners() {
   return (
     <>
       <div className="PartnersMain">
-        <div className="PartnersInner"></div>
+        <div className="PartnersInner">
+          <span style={{ fontSize: 18 }}>
+            We are sponsoring the following organizations.
+          </span>
+          <div style={{ paddingTop: 30 }}>
+            <img src="images/partners/partners1.png" />
+            <img src="images/partners/partners2.png" />
+            <img src="images/partners/partners3.png" />
+            <img src="images/partners/partners4.png" />
+          </div>
+        </div>
       </div>
     </>
   );
