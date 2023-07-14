6d5af16337871bf39c53a771c81a5f6a4070e721
seohyeon <uimi3606@gmail.com>
2023-07-14 21:19:04 +0900

    FIX 라우터 병합 시도하였으나 실패

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./Screen/HomePage";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+  useNavigation,
+} from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
