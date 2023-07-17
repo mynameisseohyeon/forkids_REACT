217a543e51a9c83ad11f42967aabd913aac9c70c
seohyeon <uimi3606@gmail.com>
2023-07-18 02:43:56 +0900

    DESIGN 배경 동물 이미지 추가

--- src/Screen/HomePage.js
+++ src/Screen/HomePage.js
@@ -11,6 +11,62 @@ function HomePage() {
     <>
       <div className="Main">
         <div className="inner">
+          <img
+            src="images/decorating/background1.png"
+            style={{
+              top: 370,
+              right: 170,
+              objectFit: "contain",
+              position: "absolute",
+            }}
+          />
+          <img
+            src="images/decorating/background2.png"
+            style={{
+              top: 370,
+              left: 170,
+              objectFit: "contain",
+              position: "absolute",
+            }}
+          />
+          <img
+            src="images/decorating/background3.png"
+            style={{
+              top: 1900,
+              right: 150,
+              objectFit: "contain",
+              position: "absolute",
+            }}
+          />
+          <img
+            src="images/decorating/background6.png"
+            style={{
+              top: 3100,
+              left: 150,
+              objectFit: "contain",
+              position: "absolute",
+            }}
+          />
+          <img
+            src="images/decorating/background7.png"
+            style={{
+              top: 4400,
+              right: 250,
+              objectFit: "contain",
+              position: "absolute",
+              width: "16%",
+            }}
+          />
+          <img
+            src="images/decorating/background4.png"
+            style={{
+              top: 5000,
+              right: 200,
+              objectFit: "contain",
+              position: "absolute",
+            }}
+          />
+
           <Contents />
           <Partners />
           <Categories />
