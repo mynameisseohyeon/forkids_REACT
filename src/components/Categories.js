c4f51b9bb81f5c87888566ca9c6172513a042357
seohyeon <uimi3606@gmail.com>
2023-07-17 18:02:48 +0900

    DESIGN 이미지 삽입

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,7 @@
 import "../css/Categories.css";
 
 //인기 랭킹
-//어린이의 연령별 장난감 소개, 1-2, 3-4, 5
+//어린이의 연령별 장난감 소개, 0-1, 2, 3-4, 5, 6
 
 function Categories() {
   return (
@@ -15,10 +15,10 @@ function Categories() {
               toys loved by children everywhere.
             </span>
             <div className="RankingBox">
-              <img src="" />
-              <img src="" />
-              <img src="" />
-              <img src="" />
+              <img src="images/decorating/toys6.png" />
+              <img src="images/decorating/toys9.png" />
+              <img src="images/decorating/toys8.png" />
+              <img src="images/decorating/toys12.png" />
             </div>
           </div>
           <div className="AgeCategory">
@@ -30,11 +30,11 @@ function Categories() {
               Buy today and watch your child's happiness grow!
             </span>
             <div className="AgeCategoryBox">
-              <img src="" />
-              <img src="" />
-              <img src="" />
-              <img src="" />
-              <img src="" />
+              <img src="images/decorating/baby1.png" />
+              <img src="images/decorating/baby2.png" />
+              <img src="images/decorating/baby3.png" />
+              <img src="images/decorating/baby4.png" />
+              <img src="images/decorating/baby5.png" />
             </div>
           </div>
         </div>
